ef69ebf2b4c681eaf6b46081ce3f787e483b8087
YordanVeselinov <yordan.r.veselinov@gmail.com>
2024-08-02 19:40:44 +0300

    Added my 3rd exam prep solutions

--- 20_Exam Preparation/03_Exam Preparation/caller.py
+++ 20_Exam Preparation/03_Exam Preparation/caller.py
@@ -1,4 +1,6 @@
 import os
+from decimal import Decimal
+
 import django
 
 # Set up Django
@@ -6,5 +8,121 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Author, Review, Article
+from django.db.models import Q, Count, Avg, Sum
+
+
+# from populate_db import populate_model_with_data
+
 
 # Create queries within functions
+
+
+def get_authors(search_name=None, search_email=None) -> str:
+    if search_name is None and search_email is None:
+        return ''
+
+    if search_name is not None and search_email is None:
+        query = Q(full_name__icontains=search_name)
+    elif search_email is not None and search_name is None:
+        query = Q(email__icontains=search_email)
+    else:
+        query = Q(full_name__icontains=search_name) & Q(email__icontains=search_email)
+
+    authors = Author.objects.filter(query).order_by('-full_name')
+
+    result = []
+
+    for a in authors:
+        result.append(f'Author: {a.full_name}, email: {a.email}, status: {"Banned" if a.is_banned else "Not Banned"}')
+
+    return '\n'.join(result)
+
+
+def get_top_publisher() -> str:
+    top_publisher = Author.objects.annotate(
+        article_count=Count('articles')
+    ).order_by(
+        '-article_count',
+        'email'
+    ).first()
+
+    if top_publisher is None or top_publisher.article_count == 0:
+        return ''
+
+    return f'Top Author: {top_publisher.full_name} with {top_publisher.article_count} published articles.'
+
+
+def get_top_reviewer() -> str:
+    top_reviewer = Author.objects.annotate(
+        review_count=Count('reviews')
+    ).order_by(
+        '-review_count',
+        'email'
+    ).first()
+
+    if top_reviewer is None or top_reviewer.review_count == 0:
+        return ''
+
+    return f'Top Reviewer: {top_reviewer.full_name} with {top_reviewer.review_count} published reviews.'
+
+
+def get_latest_article() -> str:
+    last_article = Article.objects.annotate(
+        avg_reviews_rating=Avg('reviews__rating'),
+        num_reviews=Count('reviews')
+    ).order_by(
+        '-published_on'
+    ).first()
+
+    if last_article is None or not last_article.authors:
+        return ''
+
+    authors = ', '.join(last_article.authors.order_by('full_name').values_list('full_name', flat=True))
+
+    avg_reviews_rating = last_article.avg_reviews_rating if last_article.avg_reviews_rating is not None else 0.0
+
+    return f'The latest article is: {last_article.title}. ' \
+           f'Authors: {authors}. ' \
+           f'Reviewed: {last_article.num_reviews} times. ' \
+           f'Average Rating: {avg_reviews_rating:.2f}.'
+
+
+def get_top_rated_article() -> str:
+    top_rated_article = Article.objects.annotate(
+        avg_rating=Avg('reviews__rating'),
+        review_count=Count('reviews')
+    ).filter(
+        review_count__gt=0,
+    ).order_by(
+        '-avg_rating',
+        'title',
+    ).first()
+
+    if not top_rated_article:
+        return ''
+
+    avg_rating_str = f"{top_rated_article.avg_rating:.2f}"
+
+    return f'The top-rated article is: {top_rated_article.title}, ' \
+           f'with an average rating of {avg_rating_str}, ' \
+           f'reviewed {top_rated_article.review_count} times.'
+
+
+def ban_author(email=None) -> str:
+    if email is None:
+        return 'No authors banned.'
+
+    try:
+        author = Author.objects.get(email=email)
+    except Author.DoesNotExist:
+        return "No authors banned."
+
+    author.is_banned = True
+    author.save()
+
+    reviews = author.reviews.count()
+
+    author.reviews.all().delete()
+
+    return f'Author: {author.full_name} is banned! {reviews} reviews deleted.'
--- 20_Exam Preparation/03_Exam Preparation/main_app/models.py
+++ 20_Exam Preparation/03_Exam Preparation/main_app/models.py
@@ -1,3 +1,104 @@
+from django.core.validators import MaxLengthValidator, MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
+from main_app.choices import CategoryChoices
+from main_app.managers import AuthorManager
+
+
 # Create your models here.
+
+class PublishedOnBase(models.Model):
+    class Meta:
+        abstract = True
+
+    published_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+    )
+
+
+class Author(models.Model):
+    full_name = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(3),
+        ]
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_banned = models.BooleanField(
+        default=False,
+    )
+
+    birth_year = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1900),
+            MaxValueValidator(2005),
+        ]
+    )
+
+    website = models.URLField(
+        null=True,
+        blank=True,
+    )
+
+    objects = AuthorManager()
+
+
+class Article(PublishedOnBase):
+    title = models.CharField(
+        max_length=200,
+        validators=[
+            MaxLengthValidator(200),
+            MinLengthValidator(5),
+        ]
+    )
+
+    content = models.TextField(
+        validators=[
+            MinLengthValidator(10),
+        ]
+    )
+
+    category = models.CharField(
+        max_length=10,
+        choices=CategoryChoices.choices,
+        default=CategoryChoices.TECHNOLOGY,
+        validators=[
+            MaxLengthValidator(10),
+        ]
+    )
+    authors = models.ManyToManyField(
+        to=Author,
+        related_name='articles',
+    )
+
+
+class Review(PublishedOnBase):
+    content = models.TextField(
+        validators=[
+            MinLengthValidator(10)
+        ]
+    )
+
+    rating = models.FloatField(
+        validators=[
+            MaxValueValidator(5.0),
+            MinValueValidator(1.0),
+        ]
+    )
+
+    author = models.ForeignKey(
+        to=Author,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+
+    article = models.ForeignKey(
+        to=Article,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
